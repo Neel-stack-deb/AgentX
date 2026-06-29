7882e61eae0f9222d71c384b5f203ff9ef53c849
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2026-06-29 11:40:42 +0530

    Add methods to History class for adding events and artifacts

--- src/Core/history.py
+++ src/Core/history.py
@@ -5,3 +5,11 @@ from Types.Events import Event
 class History(BaseModel):
   artifacts:list[Artifact]
   events: list[Event]
+
+  def add_event(self, event: Event) ->bool:
+    self.events.append(event)
+    return True
+
+  def add_artifact(self, artifact: Artifact) -> bool:
+    self.artifacts.append(artifact)
+    return True
