89bca8271c7003891f646b05e3fd2832edc1aec1
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2026-06-30 20:31:03 +0530

    changed the domain structure so that we get the entire planStep instead of just the id now we can access the other properties of that step as well

--- src/Types/TaskExecution.py
+++ src/Types/TaskExecution.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from Types.ExecutionResponse import ExecutionResponse
+from Types.Plan import PlanStep
 from Types.Reflection import Reflection
 
 
 class TaskExecution(BaseModel):
-    step_id: str
+    step: PlanStep
     current_response: ExecutionResponse | None = None
     current_reflection: Reflection | None = None
